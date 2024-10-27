feda7b0086afce343498227f1f38c41d4d3fa44a
Alan dos Reis <alanzit0xz@gmail.com>
2024-10-26 22:01:18 -0300

    add docker-compose

--- Dockerfile
+++ Dockerfile
@@ -2,15 +2,15 @@ FROM python:3.11-slim
 
 WORKDIR /app
 
-COPY requirements.txt .
+COPY requirements.txt . 
 
 # Instala as dependências
 RUN pip install --no-cache-dir -r requirements.txt
 
 # Copia o código da aplicação para o container
-COPY ./app/backend /app/backend
+COPY ./app/backend /app/backend  
 
-EXPOSE 5000
+EXPOSE 5000 
 
 # Comando para iniciar a aplicação Flask
 CMD ["python", "backend/app.py"]
--- app/backend/app.py
+++ app/backend/app.py
@@ -1,10 +1,33 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
+
+# Carrega as variáveis de ambiente do arquivo .env
+load_dotenv()
 
 app = Flask(__name__)
+
+# Configuração do banco de dados
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Inicializa o SQLAlchemy
+db = SQLAlchemy(app)
+
+# Modelo de Aluno
+class Aluno(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(50), nullable=False)
 
 @app.route('/')
 def hello():
     return "Hello, SchoolOps!"
+
+@app.route('/alunos', methods=['GET'])
+def get_alunos():
+    alunos = Aluno.query.all()
+    return jsonify([{'id': aluno.id, 'nome': aluno.nome} for aluno in alunos])
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
